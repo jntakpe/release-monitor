2c7b164851b332634a406859d588bb8ccd5c8470
jntakpe <jntakpe@gmail.com>
2017-10-25 19:00:36 +0200

    Lock Jetty start and stop

--- src/test/kotlin/com/github/jntakpe/releasemonitor/config/ConfigListener.kt
+++ src/test/kotlin/com/github/jntakpe/releasemonitor/config/ConfigListener.kt
@@ -5,28 +5,37 @@ import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.ContextClosedEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
+import java.util.concurrent.locks.Lock
+import java.util.concurrent.locks.ReentrantLock
 
 @Component
 class ConfigListener {
 
     companion object {
         private val wireMockServer = WireMockServer(8089)
+        private val startLock = ReentrantLock()
+        private val stopLock = ReentrantLock()
     }
 
     @EventListener
     fun handleContextRefresh(event: ApplicationReadyEvent) {
-        if (!isWiremockRunning()) {
-            wireMockServer.start()
-        }
+        startLock.run { if (!isWiremockRunning()) wireMockServer.start() }
     }
 
     @EventListener
     fun handleContextRefresh(event: ContextClosedEvent) {
-        if (isWiremockRunning()) {
-            wireMockServer.stop()
-        }
+        stopLock.run { if (isWiremockRunning()) wireMockServer.stop() }
     }
 
     private fun isWiremockRunning() = wireMockServer.isRunning
+
+    private fun Lock.run(body: () -> Unit) {
+        lock()
+        try {
+            body()
+        } finally {
+            unlock()
+        }
+    }
 
 }
